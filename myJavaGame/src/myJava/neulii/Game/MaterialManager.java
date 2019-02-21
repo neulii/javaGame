5c8d008cf80e88106e64e335b4151ca587ceb090
neulii <neulii@gmx.at>
2019-02-21 14:56:34 +0100

    added money in materialmanager

--- myJavaGame/src/myJava/neulii/Game/MaterialManager.java
+++ myJavaGame/src/myJava/neulii/Game/MaterialManager.java
@@ -2,26 +2,43 @@ package myJava.neulii.Game;
 
 public class MaterialManager {
 	
-	double coal;
-	double iron_ore;
+	private int money;
+	private int coal;
+	private int iron_ore;
 	
 	public MaterialManager() {
 		this.coal = 0;
 		this.iron_ore = 0;
 	}
 	
-	public void addMaterials(double coal, double iron_ore) {
+	public void addMaterials(int coal, int iron_ore) {
 		this.coal = this.coal + coal;
 		this.iron_ore = this.iron_ore + iron_ore;
 		
 	}
 	
-	double getIronOre() {
+	public int getIronOre() {
 		return this.iron_ore;
 	}
 	
-	public double getcoal() {
+	public int getcoal() {
 		return this.coal;
+	}
+	
+	public int getMoney() {
+		return this.money;
+	}
+	
+	public void addMoney(int money) {
+		this.money = this.money + money;
+	}
+	
+	public void subMoney(int money) {
+		if(this.money>= money) {
+			this.money = this.money - money;
+		}
+		else 
+			return;
 	}
 
 }
