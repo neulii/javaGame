480b468b6b9b91b97f9c758f64049123de5b0d9e
neulii <neulii@gmx.at>
2019-04-08 19:19:53 +0200

    fehler beseitigt im interface

--- src/myJava/neulii/Game/GameObject.java
+++ src/myJava/neulii/Game/GameObject.java
@@ -5,8 +5,8 @@ import java.awt.Graphics;
 public interface GameObject {
 
 	//Method to Render graphics
-	public void render(Graphics g);
+	void render(Graphics g);
 	
 	//Method for Updating game logic
-	public void update(long dT);
+	void update(long dT);
 }
